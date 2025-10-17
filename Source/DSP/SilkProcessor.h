5384a9168a63e8d0c1b383348724a70dfe6a1614
D3rrick La <lader@sheridancollege.ca>
2025-10-16 22:32:45 -0400

    Created .h file for Silk Processor dsp

--- Source/DSP/SilkProcessor.h
+++ Source/DSP/SilkProcessor.h
@@ -9,3 +9,37 @@
 */
 
 #pragma once
+enum SilkMode { SILK_OFF, SILK_RED, SILK_BLUE };
+
+class SilkProcessor
+{
+public:
+	SilkProcessor();
+	~SilkProcessor();
+
+	void prepare(float sr);
+	void setMode(SilkMode m);
+	void setTexture(float t);
+	float processSample(float input);
+
+private:
+	SilkMode mode;
+	float texture;
+	float sampleRate;
+	OnePoleBandpass bpRed, bpBlue;
+};
+
+class OnePoleBandpass
+{
+public:
+	OnePoleBandpass();
+	~OnePoleBandpass();
+
+	void set(float centerHz, float qVal, float sr);
+	float process(float x);
+
+private:
+	float center, q, q, alpha;
+	float sampleRate;
+	float lpState;
+};
