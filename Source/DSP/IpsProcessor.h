063d89d57d36b669bfa09c640f8f60f4eabd18c1
D3rrick La <lader@sheridancollege.ca>
2025-10-22 19:49:37 -0400

    Working on IPS Processor

--- Source/DSP/IpsProcessor.h
+++ Source/DSP/IpsProcessor.h
@@ -9,3 +9,35 @@
 */
 
 #pragma once
+enum IPSMode { IPS_7_5, IPS_15, IPS_30 };
+
+class BiQuad
+{
+public:
+	BiQuad();
+	~BiQuad();
+    void setCoeffs(float b0, float b1, float b2, float a1, float a2);
+    float process(float x);
+
+private:
+    float b0 = 1, b1 = 0, b2 = 0, a1 = 0, a2 = 0;
+    float x1 = 0, x2 = 0, y1 = 0, y2 = 0;
+};
+
+class IPSProcessor
+{
+public:
+	IPSProcessor();
+	~IPSProcessor();
+    void preprae(double sr);
+    void setMode(IPSMode m);
+    float processSample(float x);
+
+
+private:
+    IPSMode mode;
+    double sampleRate;
+    BiQuad bqLow15, bqHi15, bqLow30, bqHi30;
+
+    void buildPresets();
+};
